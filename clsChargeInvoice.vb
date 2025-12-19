efc4a85230bae2a57517a921fae7af1bb24b63eb
Michael Cuison <77145878+mtcuison@users.noreply.github.com>
2025-12-19 16:37:16 +0800

    20251219

---
+++ clsChargeInvoice.vb
@@ -0,0 +1,274 @@
+﻿Imports MySql.Data.MySqlClient
+Imports ADODB
+Imports ggcAppDriver
+Imports CrystalDecisions.CrystalReports.Engine
+Imports CrystalDecisions.[Shared].Json
+
+Public Class clsChargeInvoice
+    Private Const xsSignature As String = "08220326"
+
+    Private p_oDriver As ggcAppDriver.GRider
+    Private p_oSTRept As DataSet
+    Private p_oDTSrce As DataTable
+
+    Private p_sMachinex As String
+    Private p_nReptType As Integer      '0=Summary;1=Detail
+    Private p_sBranchCD As String
+    Private p_sTerminal As String
+
+    Private p_dFromDate As Date
+    Private p_dThruDate As Date
+
+    Private p_sRiderID As String
+
+    Public Function ReportTrans() As Boolean
+        Dim loForm As frmChargeCriteria
+        loForm = New frmChargeCriteria
+        loForm.GRider = p_oDriver
+        loForm.FormType = 0
+        loForm.HasSummaryReport = True
+        loForm.ShowDialog()
+
+        If Not loForm.isOkey Then
+            MsgBox("Unable to generate report.", MsgBoxStyle.Information, "Notice")
+            loForm = Nothing
+            Return False
+        End If
+
+        p_sTerminal = loForm.TerminalNo
+        p_sRiderID = loForm.txtField00.Tag
+        p_dFromDate = loForm.txtField00.Text
+        p_dThruDate = loForm.txtField01.Text
+        p_nReptType = loForm.ReportType
+
+        Dim oProg As frmProgress
+        oProg = New frmProgress
+        oProg.PistonInfo = p_oDriver.AppPath & "/piston.avi"
+        oProg.ShowTitle("EXTRACTING RECORDS FROM DATABASE")
+        oProg.ShowProcess("Please wait...")
+        oProg.Show()
+
+        Dim lsSQL As String
+
+        If p_nReptType = 0 Then ' Summary Report
+            lsSQL = "SELECT" &
+                    " a.`sTransNox`" &
+                    ", a.`sChargeNo`" &
+                    ", b.`dTransact`" &
+                    ", a.`sClientNm`" &
+                    ", a.`sAddressx`" &
+                    ", a.`nAmountxx`" &
+                    ", b.`sCashierx` " &
+                    "FROM Charge_Invoice a " &
+                    " LEFT JOIN SO_Master b  ON a.`sSourceNo` = b.`sTransNox`" &
+                    "          WHERE b.sTransNox LIKE " & strParm(p_oDriver.BranchCode + p_sTerminal + "%") &
+                    "            AND b.dTransact BETWEEN " & dateParm(p_dFromDate) & " AND " & dateParm(p_dThruDate)
+
+
+        Else 'Detailed Report
+            lsSQL = "SELECT" &
+                    " a.`sTransNox` " &
+                    ", a.`sChargeNo`" &
+                    ", b.`dTransact`" &
+                    ", a.`sClientNm`" &
+                    ", d.`sBarcodex`" &
+                    ", d.`sDescript`" &
+                    ", e.`sDescript` xCategryNm" &
+                    ", c.`nUnitPrce`" &
+                    ", c.`nQuantity`" &
+                    ",(c.`nUnitPrce` * c.`nQuantity`) xTranTotl" &
+                    ", b.`sCashierx` " &
+                    "FROM Charge_Invoice a " &
+                    "    LEFT JOIN SO_Master b  ON a.`sSourceNo` = b.`sTransNox`" &
+                    "    LEFT JOIN SO_Detail c ON b.`sTransNox` = c.`sTransNox`" &
+                    "    LEFT JOIN Inventory d ON c.`sStockIDx` = d.`sStockIDx`" &
+                    "    LEFT JOIN Product_Category e ON d.`sCategrID` = e.`sCategrCd` " &
+                    "        WHERE c.`cReversex` = '+'" &
+                    "           AND b.sTransNox LIKE " & strParm(p_oDriver.BranchCode + p_sTerminal + "%") &
+                    "            AND b.dTransact BETWEEN " & dateParm(p_dFromDate) & " AND " & dateParm(p_dThruDate)
+        End If
+
+
+        Debug.Print(lsSQL)
+        p_oDTSrce = p_oDriver.ExecuteQuery(lsSQL)
+
+        If p_oDTSrce.Rows.Count = 0 Then
+            MsgBox("No records found For the given criteria.", MsgBoxStyle.Information, "Notice")
+            oProg.Close()
+            Return False
+
+        End If
+
+        Dim loDtaTbl As DataTable = getRptTable()
+        Dim lnCtr As Integer
+        Dim lbAdd As Boolean = False
+
+        oProg.ShowTitle("LOADING RECORDS")
+        oProg.MaxValue = p_oDTSrce.Rows.Count
+
+
+        For lnCtr = 0 To p_oDTSrce.Rows.Count - 1
+            oProg.ShowProcess("Loading " & p_oDTSrce(lnCtr).Item("sClientNm") & "...")
+
+
+            loDtaTbl.Rows.Add(addRow(lnCtr, loDtaTbl))
+        Next
+
+        oProg.ShowSuccess()
+
+        Dim clsRpt As clsReports
+        clsRpt = New clsReports
+        clsRpt.GRider = p_oDriver
+        'Set the Report Source Here
+        If p_nReptType = 0 Then ' Summary Report    
+            If Not clsRpt.initReport("CISum") Then
+                Return False
+            End If
+        Else
+            If Not clsRpt.initReport("CIDet") Then
+                Return False
+            End If
+        End If
+        Dim loRpt As ReportDocument = clsRpt.ReportSource
+
+        Dim loTxtObj As CrystalDecisions.CrystalReports.Engine.TextObject
+        loTxtObj = loRpt.ReportDefinition.Sections(0).ReportObjects("txtCompany")
+        loTxtObj.Text = "Los Pedritos"
+
+        'Set Branch Address
+        loTxtObj = loRpt.ReportDefinition.Sections(0).ReportObjects("txtAddress")
+        loTxtObj.Text = p_oDriver.BranchName & vbCrLf & p_oDriver.Address & vbCrLf & p_oDriver.TownCity & " " & p_oDriver.ZippCode & vbCrLf & p_oDriver.Province
+
+        ''Set First Header
+        If p_nReptType = 0 Then ' Summary Report    
+            loTxtObj = loRpt.ReportDefinition.Sections(1).ReportObjects("txtHeading1")
+            loTxtObj.Text = "Charge Invoice Summarized Report"
+        Else
+            loTxtObj = loRpt.ReportDefinition.Sections(1).ReportObjects("txtHeading1")
+            loTxtObj.Text = "Charge Invoice Detailed Report"
+        End If
+
+        'Set Second Header
+        loTxtObj = loRpt.ReportDefinition.Sections(1).ReportObjects("txtHeading2")
+        loTxtObj.Text = Format(p_dFromDate, xsDATE_LONG) & " to " & Format(p_dThruDate, xsDATE_LONG)
+
+        loTxtObj = loRpt.ReportDefinition.Sections(3).ReportObjects("txtRptUser")
+        loTxtObj.Text = Decrypt(p_oDriver.UserName, "08220326")
+
+        loRpt.SetDataSource(p_oSTRept)
+        clsRpt.showReport()
+
+        Return True
+    End Function
+
+    Private Function getRptTable() As DataTable
+        'Initialize DataSet
+        p_oSTRept = New DataSet
+
+        'Load the data structure of the Dataset
+        'Data structure was saved at DataSet1.xsd 
+        p_oSTRept.ReadXmlSchema(p_oDriver.AppPath & "\vb.net\RetMgtSys\Reports\DataSet1.xsd")
+
+        'Return the schema of the datatable derive from the DataSet 
+        Return p_oSTRept.Tables(0)
+    End Function
+
+    Private Function addRow(ByVal lnRow As Integer, ByVal foSchemaTable As DataTable) As DataRow
+        'ByVal foDTInclue As DataTable
+        Dim loDtaRow As DataRow
+
+
+
+        'Create row based on the schema of foSchemaTable
+        loDtaRow = foSchemaTable.NewRow
+        If p_nReptType = 0 Then
+            loDtaRow.Item("sField01") = p_oDTSrce(lnRow).Item("sTransNox")
+            loDtaRow.Item("sField02") = Format(p_oDTSrce(lnRow).Item("dTransact"), "MMMM dd, yyyy")
+            loDtaRow.Item("sField03") = p_oDTSrce(lnRow).Item("sChargeNo")
+            loDtaRow.Item("sField04") = p_oDTSrce(lnRow).Item("sClientNm")
+            loDtaRow.Item("sField05") = p_oDTSrce(lnRow).Item("sAddressx")
+            loDtaRow.Item("sField06") = getCashier(p_oDTSrce(lnRow).Item("sCashierx"))
+            loDtaRow.Item("nField01") = CDbl(p_oDTSrce(lnRow).Item("nAmountxx"))
+            Return loDtaRow
+        Else
+            loDtaRow.Item("sField01") = p_oDTSrce(lnRow).Item("sTransNox")
+            loDtaRow.Item("sField02") = Format(p_oDTSrce(lnRow).Item("dTransact"), "MMMM dd, yyyy")
+            loDtaRow.Item("sField03") = p_oDTSrce(lnRow).Item("sChargeNo")
+            loDtaRow.Item("sField04") = p_oDTSrce(lnRow).Item("xCategryNm")
+            loDtaRow.Item("sField05") = p_oDTSrce(lnRow).Item("sClientNm")
+            loDtaRow.Item("sField06") = p_oDTSrce(lnRow).Item("sBarcodex")
+            loDtaRow.Item("sField07") = p_oDTSrce(lnRow).Item("sDescript")
+            loDtaRow.Item("lField01") = CDbl(p_oDTSrce(lnRow).Item("nUnitPrce"))
+            loDtaRow.Item("lField02") = CDbl(p_oDTSrce(lnRow).Item("nQuantity"))
+            loDtaRow.Item("lField03") = CDbl(p_oDTSrce(lnRow).Item("xTranTotl"))
+            Return loDtaRow
+        End If
+    End Function
+
+    Public Function getCashier(ByVal sCashierx As String) As String
+        Dim lsSQL As String
+        Dim lsCashierNm As String
+        Dim loDta As DataTable
+
+        lsSQL = "SELECT" &
+                    " a.sUserName" &
+                    " FROM xxxSysUser a" &
+                    " WHERE a.sUserIDxx = " & strParm(sCashierx)
+
+        loDta = p_oDriver.ExecuteQuery(lsSQL)
+
+        If loDta.Rows.Count = 0 Then
+            lsCashierNm = ""
+        Else
+            lsCashierNm = Decrypt(loDta(0).Item("sUserName"), xsSignature)
+        End If
+
+        loDta = Nothing
+
+        Return lsCashierNm
+    End Function
+
+    Public Sub New(ByVal foRider As GRider)
+        p_oDriver = foRider
+        p_oSTRept = Nothing
+        p_oDTSrce = Nothing
+    End Sub
+
+    Public Sub New(ByVal foRider As GRider,
+                   ByVal foMachineNo As String)
+        p_oDriver = foRider
+        p_oSTRept = Nothing
+        p_oDTSrce = Nothing
+
+        p_sMachinex = foMachineNo
+    End Sub
+
+    Function TranStatus(ByVal fnStatus As Int32) As String
+        If fnStatus = 0 Then
+            Return "OPEN"
+        ElseIf fnStatus = 1 Then
+            Return "APPROVED"
+        ElseIf fnStatus = 2 Then
+            Return "FULLY PAID"
+        ElseIf fnStatus = 3 Then
+            Return "DISAPPROVED"
+        ElseIf fnStatus = 4 Then
+            Return "VOID"
+        Else
+            Return "UNKNOWN"
+        End If
+    End Function
+
+    Function DetailTranStatus(ByVal fnStatus As Int32) As String
+        If fnStatus = 0 Then
+            Return "UNPAID"
+        ElseIf fnStatus = 1 Then
+            Return "PAID"
+
+        Else
+            Return "UNKNOWN"
+        End If
+    End Function
+End Class
+
+
---
+++ fmChargeCriteria.vb
@@ -0,0 +1,152 @@
+﻿Imports System.Windows.Forms
+Imports ggcAppDriver
+
+Public Class frmChargeCriteria
+    Private pn_Loaded As Integer
+    Private p_bCancelled As Boolean
+    Private p_oDriver As ggcAppDriver.GRider
+    Private p_sTerminal As String
+    Private p_sForm As String
+    Private p_oIDNumber As String
+    Private p_bHasSummary As Boolean = False
+    Private p_sReportType As Integer = 1 '0=Summary;1=Detail    
+
+    Public Property FormType As String
+        Get
+            Return p_sForm
+        End Get
+        Set(ByVal value As String)
+            p_sForm = value
+        End Set
+    End Property
+    Public Property HasSummaryReport As Boolean
+        Get
+            Return p_bHasSummary
+        End Get
+        Set(ByVal value As Boolean)
+            p_bHasSummary = value
+        End Set
+    End Property
+
+    Public Function isOkey() As Boolean
+        Return Not p_bCancelled
+    End Function
+
+    Public WriteOnly Property GRider() As ggcAppDriver.GRider
+        Set(ByVal foValue As ggcAppDriver.GRider)
+            p_oDriver = foValue
+        End Set
+    End Property
+
+    Public ReadOnly Property Cancelled() As Boolean
+        Get
+            Return p_bCancelled
+        End Get
+    End Property
+
+    Public ReadOnly Property ReportType() As Integer
+        Get
+            Return p_sReportType
+        End Get
+    End Property
+
+    Public ReadOnly Property TerminalNo As String
+        Get
+            Return p_sTerminal
+        End Get
+    End Property
+
+    Public ReadOnly Property IDNoxxx As String
+        Get
+            Return p_oIDNumber
+        End Get
+    End Property
+
+    Private Sub cmdButton01_Click(sender As Object, e As EventArgs) Handles cmdButton01.Click
+        If Not (IsDate(txtField00.Text) And
+            IsDate(txtField01.Text)) Then
+            MsgBox("There are invalid date in the RANGE group." & vbCrLf &
+                   "Please check your entry try again!", vbOKOnly, "Parameter Validation")
+            Exit Sub
+        ElseIf CDate(txtField00.Text) > CDate(txtField01.Text) Then
+            MsgBox("FROM parameter seems to be higher than THRU in the RANGE group." & vbCrLf &
+                   "Please check your entry try again!", vbOKOnly, "Parameter Validation")
+            Exit Sub
+        End If
+
+        p_bCancelled = False
+        Me.Hide()
+
+    End Sub
+
+    Private Sub cmdButton00_Click(sender As Object, e As EventArgs) Handles cmdButton00.Click
+        p_bCancelled = True
+        Me.Hide()
+    End Sub
+
+    Private Sub txtField00_Validated(ByVal sender As Object, ByVal e As System.EventArgs) Handles txtField00.Validated
+        If IsDate(txtField00.Text) Then
+            txtField00.Text = Format(CDate(txtField00.Text), "yyyy-MM-dd")
+        Else
+            txtField00.Text = Format(Now(), "yyyy-MM-dd")
+        End If
+    End Sub
+
+    Private Sub txtField01_Validated(ByVal sender As Object, ByVal e As System.EventArgs) Handles txtField01.Validated
+        If IsDate(txtField01.Text) Then
+            txtField01.Text = Format(CDate(txtField01.Text), "yyyy-MM-dd")
+        Else
+            txtField01.Text = Format(Now(), "yyyy-MM-dd")
+        End If
+    End Sub
+
+    Private Sub frmSalesCriteria_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        txtField00.Text = Format(Now(), "yyyy-MM-dd")
+        txtField01.Text = Format(Now(), "yyyy-MM-dd")
+
+        If p_sForm <> "1" Then
+            Me.Hide()
+            Call frmTerminalSelector()
+            p_bCancelled = False
+
+
+        End If
+        Me.Show()
+    End Sub
+
+    Private Sub frmTerminalSelector()
+        Dim loForm As frmTerminalSelector
+
+        loForm = New frmTerminalSelector
+        loForm.GRider = p_oDriver
+        loForm.ShowDialog()
+
+        p_sTerminal = loForm.txtField00.Text
+        p_oIDNumber = loForm.IDNumber
+
+        If loForm.Cancelled Then
+            MsgBox("Unable to generate report.", MsgBoxStyle.Information, "Notice")
+            p_bCancelled = loForm.Cancelled
+            loForm = Nothing
+            Return
+        End If
+
+    End Sub
+
+    Private Sub frmSalesCriteria_Activated(sender As Object, e As EventArgs) Handles Me.Activated
+        rbtTypex01.Enabled = p_bHasSummary
+        rbtTypex02.Checked = True
+    End Sub
+
+    Private Sub rbtTypex01_CheckedChanged(sender As Object, e As EventArgs) Handles rbtTypex01.CheckedChanged, rbtTypex02.CheckedChanged
+        If rbtTypex01.Checked Then
+            rbtTypex02.Checked = False
+            p_sReportType = 0 'Summary
+        ElseIf rbtTypex02.Checked Then
+            rbtTypex01.Checked = False
+            p_sReportType = 1 'Detailed
+
+        End If
+
+    End Sub
+End Class
